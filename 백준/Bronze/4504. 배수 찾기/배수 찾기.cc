ea273dfca2f2bfc2634c155cea1f939949278744
reneejoo22 <139679194+reneejoo22@users.noreply.github.com>
2024-08-11 00:07:01 +0900

    [Bronze III] Title: 배수 찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4504. 배수 찾기/배수 찾기.cc
@@ -0,0 +1,24 @@
+//2997
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	while (true) {
+		int num;
+		cin >> num;
+
+		if (num == 0) {
+			break;
+		}
+
+		if (num % n == 0) {
+			cout << num << " is a multiple of " << n << ".\n";
+		}
+		else {
+			cout << num << " is NOT a multiple of " << n << ".\n";
+		}
+	}
+}
