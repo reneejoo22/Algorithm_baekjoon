b157c2469afcccd0d1938ed846c68cf2b2701481
reneejoo22 <139679194+reneejoo22@users.noreply.github.com>
2024-11-02 22:07:12 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void Solve() {
+    int X, Y;
+    cin >> X >> Y;
+    cout << "yes" << "\n";
+}
+
+void TC() {
+    int T;
+    cin >> T;
+    for (int i = 0; i < T; i++) {
+        Solve();
+    }
+}
+
+int main() {
+    TC();
+}
