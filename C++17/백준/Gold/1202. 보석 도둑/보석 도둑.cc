f5f21cb721c0da1167b0182782fcde00312a772f
reneejoo22 <139679194+reneejoo22@users.noreply.github.com>
2025-02-17 12:41:37 +0900

    [Gold II] Title: 보석 도둑, Time: 424 ms, Memory: 8624 KB -BaekjoonHub

--- C++17/백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ C++17/백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,47 +1,44 @@
+//1202
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <queue>
 using namespace std;
 
-bool compareP(pair<int, int> a, pair<int, int> b) {
-    return a.second < b.second;
-} // 보석 무게 기준 오름차순 정렬
-
 int main() {
-    int N, K; // 보석 수, 가방 개수
-    cin >> N >> K;
+	int N, K; //보석 수, 가방 갯수
+	cin >> N >> K;
 
-    vector<pair<int, int>> dia(N);
-    for (int i = 0; i < N; i++) {
-        int m, v; // 무게, 가격
-        cin >> m >> v;
-        dia[i] = {v, m}; 
-    }
+	vector<pair<int, int>> dia(N);
+	for (int i = 0;i < N;i++) {
+		int m, v; //무게, 가격
+		cin >> m >> v;
+		dia[i] = { m , v }; 
+	}
 
-    vector<int> bag(K);
-    for (int p = 0; p < K; p++) {
-        cin >> bag[p];
-    }
+	vector<int>bag(K);
+	for (int p = 0;p < K;p++) {
+		cin >> bag[p];
+	}
 
-    sort(dia.begin(), dia.end(), compareP); // 보석 무게 오름차순 정렬
-    sort(bag.begin(), bag.end());           // 가방 무게 오름차순 정렬
+	sort(dia.begin(), dia.end());	//오름차순 정렬// 계속 무게 맞나 비교가 비효율적
+	sort(bag.begin(), bag.end());
 
-    priority_queue<int> inBag;
-    long long totalPrice = 0;
-    int idx = 0;
+	priority_queue<int>inBag;
+	long long totalPrice = 0;
+	int idx = 0;
 
-    for (int q = 0; q < K; q++) {
-        while (idx < N && dia[idx].second <= bag[q]) { 
-            inBag.push(dia[idx].first); // 가격을 넣음 (최대 힙)
-            idx++;
-        }
-        
-        if (!inBag.empty()) {
-            totalPrice += inBag.top(); // 가장 비싼 보석을 선택
-            inBag.pop();
-        }
-    }
-
-    cout << totalPrice;
+	for (int q = 0;q < K;q++) {
+		while (idx < N && dia[idx].first <= bag[q]) {
+			inBag.push(dia[idx].second);
+			idx++;
+		}
+		//시간초과 문제 때문에 담을 수 있는것만 따로 뺌
+		if (!inBag.empty()) {
+			totalPrice += inBag.top();
+			inBag.pop();
+		}
+	}
+	
+	cout << totalPrice;
 }
