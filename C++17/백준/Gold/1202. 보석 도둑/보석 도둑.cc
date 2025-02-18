2b433af1ea8ee1bdf538dc6afb10e800aea65482
reneejoo22 <139679194+reneejoo22@users.noreply.github.com>
2025-02-18 16:42:56 +0900

    [Gold II] Title: 보석 도둑, Time: 416 ms, Memory: 8624 KB -BaekjoonHub

--- C++17/백준/Gold/1202. 보석 도둑/보석 도둑.cc
+++ C++17/백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -1,4 +1,4 @@
-//1202
+//19941
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -6,39 +6,37 @@
 using namespace std;
 
 int main() {
-	int N, K; //보석 수, 가방 갯수
-	cin >> N >> K;
+	int N, K;
+	cin >> N >> K;  //보석 개수, 가방 개수
 
-	vector<pair<int, int>> dia(N);
+	vector<pair<int, int>>dia(N);
 	for (int i = 0;i < N;i++) {
-		int m, v; //무게, 가격
-		cin >> m >> v;
-		dia[i] = { m , v }; 
+		cin >> dia[i].first >> dia[i].second;  //무게, 가격
 	}
 
 	vector<int>bag(K);
-	for (int p = 0;p < K;p++) {
-		cin >> bag[p];
+	for (int i = 0;i < K;i++) {
+		cin >> bag[i];
 	}
 
-	sort(dia.begin(), dia.end());	//오름차순 정렬// 계속 무게 맞나 비교가 비효율적
+	sort(dia.begin(), dia.end());
 	sort(bag.begin(), bag.end());
 
-	priority_queue<int>inBag;
-	long long totalPrice = 0;
 	int idx = 0;
-
-	for (int q = 0;q < K;q++) {
-		while (idx < N && dia[idx].first <= bag[q]) {
+	long totalPrice = 0;
+	priority_queue<int>inBag;
+	for (int p = 0;p < K;p++) { //가방이 돎. 보석은 통과될때만 인덱스 넘기기
+		while(idx<N && bag[p] >= dia[idx].first){
 			inBag.push(dia[idx].second);
 			idx++;
 		}
-		//시간초과 문제 때문에 담을 수 있는것만 따로 뺌
+
 		if (!inBag.empty()) {
 			totalPrice += inBag.top();
 			inBag.pop();
 		}
 	}
-	
+
+
 	cout << totalPrice;
 }
