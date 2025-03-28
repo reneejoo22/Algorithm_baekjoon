9cd9b8fec1367fb0990eb42b709f6690e108ea2f
reneejoo22 <139679194+reneejoo22@users.noreply.github.com>
2025-03-28 11:32:00 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.cc
@@ -0,0 +1,25 @@
+//11508
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	vector<pair<int,int>>v(n);
+	for (int i = 0;i < n;i++) {
+		cin >> v[i].first >> v[i].second;
+	}
+
+	sort(v.begin(), v.end());  // 내림차순 
+
+	int time = 0;
+	for (int i = 0;i < n; i++) {
+		time = max(time, v[i].first);
+		time += v[i].second;
+	}
+
+	cout << time;
+}
