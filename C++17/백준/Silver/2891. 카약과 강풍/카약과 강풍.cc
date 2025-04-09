c114d90e228a94d320a98146caa0433fd67a0fb0
reneejoo22 <139679194+reneejoo22@users.noreply.github.com>
2025-04-09 16:49:16 +0900

    [Silver V] Title: 카약과 강풍, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/2891. 카약과 강풍/카약과 강풍.cc
@@ -0,0 +1,41 @@
+// 17420
+#include <iostream>
+#include <vector>
+#include<algorithm>
+using namespace std;
+
+int main() {
+
+    int n, s, r;
+    cin >> n >> s >> r;
+
+    vector<int> team(n, 1);
+    while (s--) { // 없는 팀 갱신
+        int temp; cin >> temp;
+        team[temp - 1]--;  // 없는 팀은 0
+    }
+    while (r--) { // 여분 팀 갱신
+        int temp; cin >> temp;
+        team[temp - 1]++;  // 여분 가져온 망가진 팀은 본인이 충족
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; i++) {
+        if (team[i] > 1) { // 본인이 여분이 있을 때
+            if (i - 1 > -1 && team[i - 1] < 1) {
+                team[i - 1]++;
+                team[i]--;
+            }
+            else if (i + 1 < n && team[i + 1] < 1) {
+                team[i + 1]++;
+                team[i]--;
+            }
+        }
+    }
+
+    for (auto now : team) {
+        if (now == 0) ans++;
+    }
+
+    cout << ans<<endl;
+}
