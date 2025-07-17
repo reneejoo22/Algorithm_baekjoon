49f2266e9c0aea1513474ce9c1ce422e27a95372
reneejoo22 <139679194+reneejoo22@users.noreply.github.com>
2025-07-17 09:28:07 +0900

    [Bronze II] Title: 중복을 없애자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/4592. 중복을 없애자/중복을 없애자.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int N, num, prevNum;
+int main()
+{
+    ios::sync_with_stdio(0), cin.tie(0);
+
+    while (true)
+    {
+        cin >> N;
+
+        if (N == 0)
+        {
+            break;
+        }
+
+        prevNum = -1;
+
+        while (N--)
+        {
+            cin >> num;
+
+            if (prevNum == num)
+            {
+                continue;
+            }
+
+            prevNum = num;
+
+            cout << num << " ";
+        }
+
+        cout << "$\n";
+    }
+    return 0;
+}
