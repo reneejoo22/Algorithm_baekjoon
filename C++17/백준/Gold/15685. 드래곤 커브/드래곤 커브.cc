56bf05f10c7fa9c144019b1e7bb1d1a635257140
reneejoo22 <139679194+reneejoo22@users.noreply.github.com>
2026-01-13 11:02:02 +0900

    [Gold III] Title: 드래곤 커브, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ C++17/백준/Gold/15685. 드래곤 커브/드래곤 커브.cc
@@ -0,0 +1,55 @@
+// 2879
+#include<iostream>
+#include<vector>
+#include <cstring>
+using namespace std;
+
+#define MAX 101
+int n;
+int x, y, d, g, cnt;
+int dy[4] = { 0,-1,0,1 };
+int dx[4] = { 1,0,-1,0 };
+int map[MAX][MAX];
+vector<int>direction;
+
+void makeDragonCurve() {
+	int size = direction.size() - 1;
+	for (int i = size; i >= 0; i--) {
+		int tmpD = (direction.at(i) + 1) % 4;
+
+		x += dx[tmpD];
+		y += dy[tmpD];
+		map[x][y] = 1;
+		direction.push_back(tmpD);
+	}
+}
+
+void coutSquare() {
+	for (int i = 0; i < MAX; i++) {
+		for (int j = 0; j < MAX; j++) {
+			if (map[i][j] && map[i + 1][j] && map[i + 1][j + 1] && map[i][j + 1])
+				cnt++;
+		}
+	}
+}
+
+int main() {
+	cin >> n;
+	while (n--) {
+		direction.clear();
+		cin >> x >> y >> d >> g;
+
+		map[x][y] = 1;
+
+		// 0 generation
+		x += dx[d];
+		y += dy[d];
+		map[x][y] = 1;
+		direction.push_back(d);
+		while (g--) {
+			makeDragonCurve();
+		}
+	}
+	coutSquare();
+	cout << cnt << endl;
+}
