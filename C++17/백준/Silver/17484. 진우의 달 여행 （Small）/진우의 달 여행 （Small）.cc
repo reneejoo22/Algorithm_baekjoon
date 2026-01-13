aebdb30a92512f266f9489e6872d981871bade7b
reneejoo22 <139679194+reneejoo22@users.noreply.github.com>
2026-01-13 09:06:49 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.cc
+++ C++17/백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.cc
@@ -6,33 +6,35 @@
 using namespace std;
 
 const int INF = 2137000000;
-int dx[3] = { -1,0,1 };
-int n, m, ans = INF, map[7][7];
+int dx[3] = { -1, 0, 1 };
+int n, m;
+int map[7][7];
+int ans = INF;
 
-int solve(int y, int x, int last) {
-	if (y == n) return 0; //끝에 도달하면 이 path는 끝
-	int ret = INF;  //끝에 도달한 후 뒤로 한발짝 가서 거기서 dx[1]로 감
+int move(int y, int x, int last) {
+	if (y == n)return 0;
+	int ret = INF;
 
 	for (int i = 0; i < 3; i++) {
-		if (last == i) continue;
-		if (x + dx[i] < 0 || x + dx[i] >= m) continue;   //범위 안 벗어나게
-		ret = min(ret, solve(y + 1, x + dx[i], i) + map[y][x]);
+		if (last == i) continue; //ㄱ같은 방향으로 연속 이동 안되기 때문에
+		int nextX = x + dx[i];
+		
+		if (nextX < 0 || nextX >= m)continue;
+		// cout << i << " i, curr "<<map[y+1][nextX] << endl;
+		ret = min(ret, move(y + 1, nextX, i)+ map[y][x]);
 	}
 	return ret;
 }
-
 int main() {
-
 	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			cin >> map[i][j];
 		}
 	}
-	for (int i = 0; i < m; i++) { //가로 길이만큼
-		ans = min(ans, solve(0, i, -1));
-	}
 
+	for (int i = 0; i < m; i++) {
+		ans= min(ans,move(0,i,-1));
+	}
 	cout << ans << endl;
-	return 0;
 }
