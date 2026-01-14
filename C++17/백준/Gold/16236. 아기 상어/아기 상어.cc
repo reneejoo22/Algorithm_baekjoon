ef15552397cc16f0e8689d302355c754f70a4f05
reneejoo22 <139679194+reneejoo22@users.noreply.github.com>
2026-01-14 09:23:12 +0900

    [Gold III] Title: 아기 상어, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- C++17/백준/Gold/16236. 아기 상어/아기 상어.cc
+++ C++17/백준/Gold/16236. 아기 상어/아기 상어.cc
@@ -7,26 +7,75 @@
 
 using namespace std;
 
-int n;
-pair<int, int>sharkPos;
-int sharkSize = 2;
-int ate = 0, ans = 0;
-int map[25][25], visited[25][25];
+int n, ans = 0;
+int sharkSize = 2, sharkX, sharkY, ate;
+int map[20][20], visited[20][20];
+int dy[4] = { -1,0,0,1 }, dx[4] = { 0,-1,1,0 };
 
-int dx[4] = { 0,1,0,-1 };
-int dy[4] = { 1,0,-1,0 };
+int bfs() {
+	for (int i = 0; i < n; i++) {
+		memset(visited[i], 0, sizeof(int) * n);
+	}
+	int dist = 0;
+	queue<pair<int, int>>q;//상어의 이동 경로를 저장한 큐
+	//상어가 먹을 수 있는 물고기 위치, pq에는 물고기까지의 거리가 최단 거리인 것만 들어감
+	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+	q.push({ sharkY,sharkX });  //상어 현재 위치
+	visited[sharkY][sharkX] = 1;  // 현재 서있으니 방문한 것
+	
+	while (!q.empty()) {
+		int y = q.front().first;
+		int x = q.front().second;
+		q.pop();
+		if (visited[y][x] == dist)
+			break;
+		for (int dir = 0; dir < 4; dir++) {
+			int ny = y + dy[dir];  //상하좌우 탐색. 현재 위치에서
+			int nx = x + dx[dir];
 
-int bfs();
+			if (ny < 0 || ny >= n || nx < 0 || nx >= n) //벗어난 경우
+				continue;  //다음 반복문
+			if (visited[ny][nx]) //방문한 경우
+				continue; // 다음 반복문ㄴ
+			if (!map[ny][nx] || map[ny][nx] == sharkSize) {
+				// 자신과 같거나 0이면 지나갈 수 있음
+				q.push({ ny,nx }); //나중에 지나가게 저장
+				visited[ny][nx] = visited[y][x] + 1; //현재 자리에 2 기록. 시간
+			}
+			else if (map[ny][nx] < sharkSize) {
+				//먹을 수 있음
+				if (!dist)
+					ate++;
+				visited[ny][nx] = visited[y][x] + 1; //현재 자리에 2 기록. 시간
+				dist = visited[ny][nx]; //최소 거리 확정
+				pq.push({ ny,nx }); // 물고기를 먹은 좌표 저장!!!@
+			}
+		}
+	}
+	if (pq.empty()) {
+		return 0;
+	}
+	sharkX = pq.top().second;
+	sharkY = pq.top().first;
 
-int main() {
+	map[sharkY][sharkX] = 0;
+	if (ate == sharkSize) {
+		sharkSize++;
+		ate = 0;
+	}
+	return visited[sharkY][sharkX] - 1;
+}
+
+void solve() {
 	cin >> n;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> map[i][j];
 			if (map[i][j] == 9) {
-				sharkPos = { i,j };
-				map[i][j] = 0;
+				sharkX = j;
+				sharkY = i;
+				map[i][j] = 0; //이동할 수 있도록
 			}
 		}
 	}
@@ -34,69 +83,14 @@ int main() {
 	while (1) {
 		int sec = bfs();
 		ans += sec;
-		if (sec == 0)
-			break;
-	}
-	cout << ans << endl;
-}
-
-int bfs() {
-	for (int i = 0; i < n; i++) {
-		memset(visited[i], 0, sizeof(int)* n);  //? memset 방법 익히기
- 	}
-
-	int dist = 0; //거리
-	queue<pair<int, int>>q; //상어의 이동 경로를 저장한 큐
-
-	// 물고기를 먹은 길을 저장할 곳 필요  // ? pq 이거 익히기
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
-
-	q.push({ sharkPos.first, sharkPos.second }); //? 이거 큐에 넣어야
-	visited[sharkPos.first][sharkPos.second] = 1;
-
-	while (!q.empty()) {
-		int y = q.front().first;
-		int x = q.front().second;
-		q.pop();
-
-		if (visited[y][x] == dist) { //? 최단거리에 있는 물고기를 다 찾았을 때
+		if (!sec) {
 			break;
 		}
-		for (int i = 0; i < 4; i++) {
-			int moveY = y + dy[i];
-			int moveX = x + dx[i];
-
-			// 가장 먼저 맵 벗어났는지 확인
-			if (moveY < 0 || moveY >= n || moveX < 0 || moveX >= n)
-				continue;
-			if (visited[moveY][moveX]) // 0 이 아니면
-				continue;
-			//지나갈 수 잇는 길이면 큐에 저장
-			if (map[moveY][moveX] == sharkSize || map[moveY][moveX] == 0) {
-				q.push({ moveY, moveX });
-				visited[moveY][moveX] = visited[y][x] + 1;
-			}
-			// 먹을 수 있는 물고기라면
-			else if (map[moveY][moveX] < sharkSize) {
-				if (dist == 0)
-					ate++; // 거리가 확정났다면 먹은 개수를 늘리지 않음
-				visited[moveY][moveX] = visited[y][x] + 1;
-				dist = visited[moveY][moveX];
-				pq.push({ moveY, moveX });
-			}
-		}
 	}
-
-	if (pq.empty()) {
-		return 0;
-	}
-	sharkPos.first = pq.top().first;
-	sharkPos.second = pq.top().second;
-	map[sharkPos.first][sharkPos.second] = 0;
-	if (ate == sharkSize) {
-		sharkSize++;
-		ate = 0;
-	}
-	return visited[sharkPos.first][sharkPos.second] - 1;
-
+	cout << ans;
 }
+
+int main() {
+	solve();
+}
+
